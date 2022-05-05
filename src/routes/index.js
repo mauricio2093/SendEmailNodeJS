5e40932384b600b59bbc8356c5f9400a1dfad3ca
mauricio2093 <mauricio2093@gmail.com>
2022-05-04 23:18:11 -0500

    Subiendo envio de correo

--- src/routes/index.js
+++ src/routes/index.js
@@ -67,48 +67,75 @@ router.post('/send-email', [
     }); 
 
     const info = await transporter.sendMail({
-        from: "ANOVA SERVER<anovasupport@anovaconsultancy.com.co>",
+        from: "Somebody<anovasupport@anovaconsultancy.com.co>",
         to: process.env.USER_R,
-        subject:'FORMULARIO DE CONTACTO ANOVA WEB',
+        subject:'CONTACT FORM USER',
         html: contentHTML
     });
     contentHTML1 = `
-        <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet">
-        <h1>Bienvenido a <span style="color:rgb(240, 206, 12); font-weight:bold;">ANOVA CONSULTANCY</span></h1>
-        <h2>Hola, ${name} <${email}><br>
-        Tu mensaje fue enviado con éxito! Pronto nos contactaremos contigo!
-        </h2>
-        <p>Enviaste el siguiente Mensaje:<br> ${message}</p>
-        <h3><br>Equipo Anova Consultancy&nbsp;</h3>
-        <p>anovaconsultancy@anovaconsultancy.com.co&nbsp;
-            <br>(+57) 3168061123
-            <br><a href="https://anovaconsultancy.com.co/">anovaconsultancy.com.co</a>
-        </p>
-        <div class="home__social">
-            <a href="https://www.facebook.com/williamarmando.bautistafarias.5" target="_blank" class="home__social-link fb">
-                <img src="https://anovaconsultancy.com.co/assets/img/icon-fb.png" alt="Logo"></figure>
+    <html><body>
+    <table width='100%' bgcolor='#000000' cellpadding='0' cellspacing='0' border='0'>
+    <tr><td>
+    <table align='center' width='100%' border='0' cellpadding='0' cellspacing='0' style='max-width:650px; background-color:#1F1F1F; font-family:Verdana, Geneva, sans-serif;'>
+                
+    <thead>
+        <tr height='80'>
+        <th colspan='4' style='background-color:#1F1F1F; border-bottom:solid 1px #bdbdbd; font-family:Verdana, Geneva, sans-serif; color:#fafa00; font-weight:normal; font-size:34px;' >Sending Email with NodeJS</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr align='center' height='50' style='font-family:Verdana, Geneva, sans-serif; >
+        <td style='background-color:#1F1F1F; text-align:center;'><a href='#' style='color:#bfbfbf; text-decoration:none;'>Information</a></td>
+        <td style='background-color:#1F1F1F; text-align:center;'><a href='#' style='color:#bfbfbf; text-decoration:none;'>Services</a></td>
+        <td style='background-color:#1F1F1F; text-align:center;'><a href='#' style='color:#bfbfbf; text-decoration:none;' >Us</a></td>
+        <td style='background-color:#1F1F1F; text-align:center;'><a href='#' style='color:#bfbfbf; text-decoration:none;' >Contact</a></td>
+        </tr>
+
+        <tr>
+        <td colspan='4' style='padding:15px; border-top:solid 1px #bdbdbd;''>
+        <p style='font-size:30px; color:#ffff;'>Hi!  <span style='color:#fafa00;text-decoration:none'>${name} &lt;${email}&gt;</span>,<br>Welcome to our <span style='color:#fafa00; font-weight:normal;'>Web Site</span>.</p>
+        <hr />
+        <p style='font-size:25px; color:#bfbfbf;'>Your message has been sent successfully! We will contact you soon!</p>
+        <p style='font-size:15px; font-family:Verdana, Geneva, sans-serif;color:#bfbfbf;'>Your Phone is: ${phone} <br> You sent the following message: <br>${message}.</p>
+        <p style='font-size:20px; color:#bfbfbf;'>Team !&nbsp;</p>
+        <p style='font-size:15px; color:#bfbfbf;  text-decoration:none;'>email&nbsp;
+        <br>(+00) 0000<br><a style='text-decoration:none;' href='https://sending.com.co/'>sending.com.co</a></p>
+        <div>
+            <a style='text-decoration:none;' href='https://www.facebook.com/' target='_blank'>
+                <figure><img src='https://anovaconsultancy.com.co/assets/img/icon-fb.png' alt='Logo'></figure>
             </a>
-            <a href="https://www.instagram.com/williamarmandobautista/" target="_blank" class="home__social-link ig">
-                <img src="https://anovaconsultancy.com.co/assets/img/icon-ig.png" alt="Logo"></figure>
+            <a  style='text-decoration:none;' href='https://www.instagram.com/' target='_blank'>
+                <figure><img src='https://anovaconsultancy.com.co/assets/img/icon-ig.png' alt='Logo'></figure>
             </a>
-            <a href="https://twitter.com/" target="_blank" class="home__social-link tw">
-                <img src="https://anovaconsultancy.com.co/assets/img/icon-tw.png" alt="Logo"></figure>
+            <a style='text-decoration:none;' href='https://twitter.com/' target='_blank' >
+                <figure><img src='https://anovaconsultancy.com.co/assets/img/icon-tw.png' alt='Logo'></figure>
             </a>
-            <a href="https://api.whatsapp.com/send?phone=573168061123" target="_blank">
-                <img src="https://anovaconsultancy.com.co/assets/img/icon-wp.png" alt="Logo"></figure>
+            <a style='text-decoration:none;' href='https://api.whatsapp.com/' target='_blank'>
+                <figure><img src='https://anovaconsultancy.com.co/assets/img/icon-wp.png' alt='Logo'></figure>
             </a>
         </div>
-        <p><br><br>&nbsp;</p><figure class="image"><img src="https://anovaconsultancy.com.co/assets/img/ABC.png" alt="Logo"></figure>
+        <p><br><br>&nbsp;</p>
+
+        </td>
+        </tr>
+
+    </tbody>
+    </table>
+
+    </td></tr>
+    </table>
+        
+    </body></html>
     `;
     const info1 = await transporter.sendMail({
-        from: "ANOVA SERVER<anovasupport@anovaconsultancy.com.co>",
+        from: "Somebody<anovasupport@anovaconsultancy.com.co>",
         to: email,
-        subject:'[CONFIRMACIÓN]: Tu mensaje has sido enviado exitosamente',
+        subject:'[CONFIRMATION]: Your messege was sent',
         html: contentHTML1
     });
-    console.log('Mensaje enviado', info.messageId);
-    console.log('Mensaje enviado', info1.messageId);
-    res.send('Tu Mensaje fue enviado con Éxito');
+    console.log('messege was sent', info.messageId);
+    console.log('messege was sent', info1.messageId);
+    res.send('Your message was sent successfully');
 })
 
 
